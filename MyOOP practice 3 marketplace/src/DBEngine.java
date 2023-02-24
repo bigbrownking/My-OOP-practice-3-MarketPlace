a42f1233a1ea36b734f0ca918ab4f6b910dc26fd
bigbrownking <121678868+bigbrownking@users.noreply.github.com>
2023-02-24 17:35:00 +0600

    Delete MyOOP practice 3 marketplace directory

--- MyOOP practice 3 marketplace/src/DBEngine.java
+++
@@ -1,5 +0,0 @@
-public class DBEngine {
-    public static final String url = "jdbc:mysql://localhost:3306/marketplace";
-    public static final String user = "root";
-    public static final String password = "12345";
-}
--- MyOOP practice 3 marketplace/src/OS/Mac.java
+++
@@ -1,8 +0,0 @@
-package OS;
-import Accessories.OS;
-public class Mac extends OS{
-    public Mac(){}
-    public Mac(String osType, int price){
-        super(osType, price);
-    }
-}
